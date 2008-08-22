e228a2cf1bbb2fcadc4e1b681ac92359a28a296d
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-08-22 01:11:49 +0000

    minor fix: OperationImpl -> UnbackedOperationImpl

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/LuceneQueryPlugin.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/LuceneQueryPlugin.java
@@ -40,9 +40,8 @@ import edu.stanford.smi.protege.query.ui.QueryUtil;
 import edu.stanford.smi.protege.resource.Icons;
 import edu.stanford.smi.protege.server.framestore.RemoteClientFrameStore;
 import edu.stanford.smi.protege.server.metaproject.Operation;
-import edu.stanford.smi.protege.server.metaproject.impl.OperationImpl;
+import edu.stanford.smi.protege.server.metaproject.impl.UnbackedOperationImpl;
 import edu.stanford.smi.protege.ui.ListFinder;
-import edu.stanford.smi.protege.util.LuceneQueryPluginDefaults;
 import edu.stanford.smi.protege.util.ComponentFactory;
 import edu.stanford.smi.protege.util.ComponentUtilities;
 import edu.stanford.smi.protege.util.DoubleClickActionAdapter;
@@ -51,6 +50,7 @@ import edu.stanford.smi.protege.util.LabeledComponent;
 import edu.stanford.smi.protege.util.ListPanel;
 import edu.stanford.smi.protege.util.ListPanelListener;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protege.util.LuceneQueryPluginDefaults;
 import edu.stanford.smi.protege.util.SelectableList;
 import edu.stanford.smi.protege.util.ViewAction;
 import edu.stanford.smi.protege.widget.AbstractTabWidget;
@@ -66,16 +66,16 @@ import edu.stanford.smi.protegex.owl.ui.icons.OverlayIcon;
  * @date 15-Aug-06
  */
 public class LuceneQueryPlugin extends AbstractTabWidget {
-    
+
     public static final int DEFAULT_MAX_MATCHES = 1000;
 
 	public static final String SEARCHING_ITEM = "Searching...";
 	private static final String SEARCH_RESULTS = "Search Results";
 	private static final String SEARCH_IN_PROGRESS = "Search Results (search in progress)";
-	
+
 	private static final long serialVersionUID = -5589620508506925170L;
 
-	public static final Operation INDEX_OPERATION = new OperationImpl("Generate Lucene Indices");
+	public static final Operation INDEX_OPERATION = new UnbackedOperationImpl("Generate Lucene Indices", "");
 
 	private KnowledgeBase kb;
 	private Collection<Slot> slots;
@@ -83,7 +83,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 	private boolean isOWL;
 	private Slot defaultSlot = null;
     private int maxMatches = DEFAULT_MAX_MATCHES;
-	
+
 	private ViewAction viewAction;
 	private ViewAction editAction;
 	private JButton viewButton;
@@ -94,7 +94,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 	private JRadioButton btnAndQuery;
 	private JRadioButton btnOrQuery;
 	private JPanel pnlQueryBottom;
-    
+
 	private JButton btnSearch;
 	private LabeledComponent resultsComponent;
 	private QueryRenderer queryRenderer;
@@ -112,19 +112,19 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 	@SuppressWarnings("unchecked")
 	public void initialize() {
 		this.kb = getKnowledgeBase();
-		
+
 		// determine if current project is an OWL project, if so collect OWLProperty values
-		this.isOWL = (getKnowledgeBase() instanceof OWLModel);
-				
-		this.queryRenderer = (this.isOWL ? new QueryResourceRenderer() : new QueryFrameRenderer());
+		this.isOWL = getKnowledgeBase() instanceof OWLModel;
+
+		this.queryRenderer = this.isOWL ? new QueryResourceRenderer() : new QueryFrameRenderer();
 		this.queryRenderer.setMatchColor(new Color(24, 72, 124));
-		
+
 		InstallNarrowFrameStore frameStore = new InstallNarrowFrameStore(kb);
 		this.canIndex = RemoteClientFrameStore.isOperationAllowed(kb, INDEX_OPERATION);
 		this.slots = (Set) frameStore.execute();
 
 		setDefaultSlot(kb.getSlot(LuceneQueryPluginDefaults.getDefaultSearchSlotName()));
-		       
+
         // add UI components
 		createGUI();
 		// add the default first query component
@@ -141,7 +141,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 
         JPanel pnlLeft = new JPanel(new BorderLayout(5, 5));
 		LabeledComponent lcLeft = new LabeledComponent("Query", pnlLeft, true);
-		
+
 		// only add if the user has permission to index the ontology
 		if (canIndex) {
 			addIndexButton(lcLeft);
@@ -161,9 +161,9 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 			btn.setMinimumSize(dim);
 			btn.setMaximumSize(dim);
 		}
-		
+
 		pnlLeft.add(new JScrollPane(getQueryList()), BorderLayout.CENTER);
-		pnlLeft.add(getQueryBottomPanel(), BorderLayout.SOUTH);		
+		pnlLeft.add(getQueryBottomPanel(), BorderLayout.SOUTH);
 
 		SelectableList lst = getResultsList();
 		resultsComponent = new LabeledComponent(SEARCH_RESULTS, new JScrollPane(lst), true);
@@ -171,13 +171,13 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 
 		viewButton = resultsComponent.addHeaderButton(getEditAction());	// won't be null
 		editButton = resultsComponent.addHeaderButton(getViewAction());	// might be null
-		
+
 		JSplitPane splitter = ComponentFactory.createLeftRightSplitPane();
 		splitter.setLeftComponent(lcLeft);
 		splitter.setRightComponent(resultsComponent);
         add(splitter, BorderLayout.CENTER);
 	}
-	
+
 	private Action getEditAction() {
 		if (editAction == null) {
 			if (NCIEditAction.isValid()) {
@@ -188,7 +188,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 		}
 		return editAction;
 	}
-	
+
 	private Action getViewAction() {
 		if (viewAction == null) {
 			if (NCIViewAction.isValid()) {
@@ -230,7 +230,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 		JButton btnTemp = lc.addHeaderButton(action);
 		btnTemp.getParent().add(btnProgress);
 		btnTemp.getParent().remove(btnTemp);	// now remove the fake button
-		
+
 		btnProgress.setToolTipText("Create the index for this ontology");
 		btnProgress.setPreferredSize(new Dimension(120, 26));
 		btnProgress.setFont(btnProgress.getFont().deriveFont(Font.BOLD));
@@ -238,7 +238,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 	}
 
 	/**
-	 * Initializes and returns the query {@link ListPanel}.  This is the panel that contains 
+	 * Initializes and returns the query {@link ListPanel}.  This is the panel that contains
 	 * all the queries (there will always be at least one query).
 	 */
 	private ListPanel getQueryList() {
@@ -260,7 +260,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 
 	/**
 	 * Initializes and returns the bottom query panel which contains
-	 * the "Add Query", "Clear" and "Search" buttons, as well as the 
+	 * the "Add Query", "Clear" and "Search" buttons, as well as the
 	 * "Match All" and "Match Any" checkboxes.
 	 */
 	private JPanel getQueryBottomPanel() {
@@ -268,10 +268,10 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 			pnlQueryBottom = new JPanel();
 			pnlQueryBottom.setLayout(new BoxLayout(pnlQueryBottom, BoxLayout.LINE_AXIS));
 			pnlQueryBottom.setPreferredSize(new Dimension(500, 28));
-			
+
 			pnlQueryBottom.add(new JButton(getSetMaxMatchesAction()));
 			pnlQueryBottom.add(Box.createRigidArea(new Dimension(4, 0)));
-			
+
 			JButton btn = new JButton(new AbstractAction("Clear", Icons.getClearIcon(false, false)) {
 				public void actionPerformed(ActionEvent e) {
 					clearComponents();
@@ -280,7 +280,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 			btn.setToolTipText("Remove all queries and start over");
 			pnlQueryBottom.add(btn);
 			pnlQueryBottom.add(Box.createRigidArea(new Dimension(8, 0)));
-			
+
 			btnAndQuery = new JRadioButton("Match All  ", true);
 			btnOrQuery = new JRadioButton("Match Any  ", false);
 			pnlQueryBottom.add(btnAndQuery);
@@ -288,9 +288,9 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 			ButtonGroup group = new ButtonGroup();
 			group.add(btnAndQuery);
 			group.add(btnOrQuery);
-			
+
 			pnlQueryBottom.add(Box.createHorizontalGlue());
-			
+
 			btnSearch = new JButton(new AbstractAction("Search", Icons.getFindIcon()) {
 				public void actionPerformed(ActionEvent e) {
 					doSearch();
@@ -305,7 +305,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 					}
 				}
 			});
-		}			
+		}
 		return pnlQueryBottom;
 	}
 
@@ -320,13 +320,14 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 
 // KLO start
 
-    @SuppressWarnings("unchecked")
+    @Override
+	@SuppressWarnings("unchecked")
 	public Collection getSelection() {
 		return lstResults.getSelection();
 	}
 
 // KLO end
-    
+
 	private Action getAddQueryAction() {
 		return new AbstractAction("Add Query", Icons.getAddQueryLibraryIcon()) {
 			public void actionPerformed(ActionEvent e) {
@@ -338,7 +339,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
     private Action getSetMaxMatchesAction() {
         return new AbstractAction("Set Max Matches") {
             public void actionPerformed(ActionEvent e) {
-                String userValue = JOptionPane.showInputDialog("Enter max matches count (0 or less for all)", 
+                String userValue = JOptionPane.showInputDialog("Enter max matches count (0 or less for all)",
                                                                new Integer(maxMatches));
                 try {
                     maxMatches = Integer.parseInt(userValue);
@@ -349,9 +350,9 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
             }
         };
     }
-    
+
 	private Action getAddRestrictionQueryAction() {
-		Icon icon = new OverlayIcon(OWLIcons.getImageIcon(OWLIcons.PRIMITIVE_OWL_CLASS).getImage(), 5, 5, 
+		Icon icon = new OverlayIcon(OWLIcons.getImageIcon(OWLIcons.PRIMITIVE_OWL_CLASS).getImage(), 5, 5,
 									OWLIcons.getImageIcon(OWLIcons.ADD_OVERLAY).getImage(), 15, 13, 15, 16);
 		return new AbstractAction("Add a nested query", icon) {
 			public void actionPerformed(ActionEvent e) {
@@ -390,7 +391,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 			editButton.setVisible(visible);
 		}
 	}
-	
+
 	private void addQueryComponent() {
 		QueryUtil.addQueryComponent(kb, slots, defaultSlot, queriesListPanel);
 	}
@@ -407,7 +408,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 
 	/**
 	 * Creates the {@link Query} object from all the {@link QueryComponent}s.
-	 * If there are multiple queries then either an {@link AndQuery} or an {@link OrQuery} are used.  
+	 * If there are multiple queries then either an {@link AndQuery} or an {@link OrQuery} are used.
 	 * Passes the {@link Query} on to {@link LuceneQueryPlugin#doQuery(Query)} if the query is valid.
 	 */
 	public void doSearch() {
@@ -415,7 +416,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 		resultsComponent.setHeaderLabel(SEARCH_IN_PROGRESS);
 		final Cursor oldCursor = getCursor();
 		setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-		lstResults.setListData(new String[] { SEARCHING_ITEM }); 
+		lstResults.setListData(new String[] { SEARCHING_ITEM });
 
 		// start searching in a new thread
 		SwingUtilities.invokeLater(new Runnable() {
@@ -423,7 +424,7 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 				int hits = 0;
 				boolean error = false;
 				try {
-					VisitableQuery query = QueryUtil.getQueryFromListPanel(queriesListPanel, 
+					VisitableQuery query = QueryUtil.getQueryFromListPanel(queriesListPanel,
 															      		   btnAndQuery.isSelected());
 					if (maxMatches > 0) {
 						query = new MaxMatchQuery(query, maxMatches);
@@ -435,14 +436,14 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 					final String msg = "Invalid query: " + e.getMessage();
 					System.err.println(msg);
 					error = true;
-					lstResults.setListData(new String[] { msg }); 
+					lstResults.setListData(new String[] { msg });
 				} catch (Exception ex) {
 					// IOException happens for "sounds like" queries when the ontology hasn't been indexed
-					final String msg = "An exception occurred during the query.\n" + 
+					final String msg = "An exception occurred during the query.\n" +
 						"This possibly happened because this ontology hasn't been indexed.\n" + ex.getMessage();
 					JOptionPane.showMessageDialog(LuceneQueryPlugin.this, msg, "Error", JOptionPane.ERROR_MESSAGE);
 					error = true;
-					lstResults.setListData(new String[] { "An exception occurred during the query." }); 
+					lstResults.setListData(new String[] { "An exception occurred during the query." });
 				} finally {
 					setCursor(oldCursor);
 					btnSearch.setEnabled(true);
@@ -453,15 +454,15 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 			}
 		});
 	}
-    
+
     private void indicateSearchDone(int hits, boolean error) {
-        String matchString = (error ? "" : "  (" + hits + " match" + (hits == 1 ? ")" : "es)"));
+        String matchString = error ? "" : "  (" + hits + " match" + (hits == 1 ? ")" : "es)");
         resultsComponent.setHeaderLabel(SEARCH_RESULTS + matchString);
     }
 
 	/**
 	 * Executes the query using the {@link KnowledgeBase} and puts the results
-	 * into the list. 
+	 * into the list.
 	 * @param q the query to perform
 	 * @see KnowledgeBase#executeQuery(Query)
 	 * @return int number of hits for the query
@@ -471,12 +472,12 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 		if (q != null) {
 			results = kb.executeQuery(q);
 		}
-		int hits = (results != null ? results.size() : 0);
+		int hits = results != null ? results.size() : 0;
 		if (hits == 0) {
 			queryRenderer.setQuery(null);	// don't bold anything
 			lstResults.setListData(new String[] { "No results found." });
 		} else {
-			queryRenderer.setQuery(q);		// bold the matching results 
+			queryRenderer.setQuery(q);		// bold the matching results
 			lstResults.setListData(new Vector<Frame>(results));
 			lstResults.setSelectedIndex(0);
 		}
@@ -491,13 +492,13 @@ public class LuceneQueryPlugin extends AbstractTabWidget {
 		if (defaultSlot == null) {
 			defaultSlot = kb.getSlot(LuceneQueryPluginDefaults.DEFAULT_SLOT_NAME);
 		}
-		
+
 		if (defaultSlot == null) {
 			this.defaultSlot = kb.getNameSlot();
 		} else {
 			this.defaultSlot = defaultSlot;
 		}
-	
+
 	}
-	
+
 }
