ea2468f82b4ac5753a7491c2b613b780d9f29f00
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-09-02 23:08:48 +0000

    this has been redesigned to ensure that the query tab is initialized when it is desired by the server administrator

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/QueryInstallerPlugin.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/QueryInstallerPlugin.java
@@ -8,23 +8,23 @@ import edu.stanford.smi.protege.model.WidgetDescriptor;
 import edu.stanford.smi.protege.plugin.ProjectPluginAdapter;
 import edu.stanford.smi.protege.query.api.QueryApi;
 import edu.stanford.smi.protege.query.api.QueryConfiguration;
+import edu.stanford.smi.protege.util.ApplicationProperties;
 import edu.stanford.smi.protege.util.Log;
 
 public class QueryInstallerPlugin extends ProjectPluginAdapter {
+	
+	public static final String AUTO_INSTALL_QUERY = "query.lucene.server.autoinstall";
+	private static boolean autoInstall = ApplicationProperties.getBooleanProperty(AUTO_INSTALL_QUERY, true);
     
     public void afterLoad(Project p) {
+    	if (!p.isMultiUserServer()) {
+    		return;
+    	}
         String pluginClassName = LuceneQueryPlugin.class.getCanonicalName();
         WidgetDescriptor desc = p.getTabWidgetDescriptor(pluginClassName);
-        if (desc.isVisible() && !p.isMultiUserServer()) {
+        if (desc != null && desc.isVisible()) {
             KnowledgeBase kb = p.getKnowledgeBase();
             new QueryApi(kb).install(new QueryConfiguration(kb));
-        }
-        Collection descriptors = p.getTabWidgetDescriptors();
-        for (Object o : descriptors) {
-            WidgetDescriptor d = (WidgetDescriptor) o;
-            if (d.isVisible()) {
-                Log.getLogger().info("descriptor = " + o + " visible = " + d.isVisible());
-            }
         }
     }
 }
