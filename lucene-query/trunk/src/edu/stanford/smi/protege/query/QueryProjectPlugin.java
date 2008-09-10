4179bee94d4cf50796fc3cd28f11ccb8e6d1fa96
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-09-10 21:17:46 +0000

    refactored the name and modified so that it uses the lucene query configuration to install the query narrow frame store.

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/QueryProjectPlugin.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/QueryProjectPlugin.java
@@ -1,30 +1,36 @@
 package edu.stanford.smi.protege.query;
 
-import java.util.Collection;
-
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Project;
 import edu.stanford.smi.protege.model.WidgetDescriptor;
 import edu.stanford.smi.protege.plugin.ProjectPluginAdapter;
 import edu.stanford.smi.protege.query.api.QueryApi;
-import edu.stanford.smi.protege.query.api.QueryConfiguration;
+import edu.stanford.smi.protege.query.menu.LuceneConfiguration;
 import edu.stanford.smi.protege.util.ApplicationProperties;
-import edu.stanford.smi.protege.util.Log;
 
-public class QueryInstallerPlugin extends ProjectPluginAdapter {
+public class QueryProjectPlugin extends ProjectPluginAdapter {
 	
 	public static final String AUTO_INSTALL_QUERY = "query.lucene.server.autoinstall";
 	private static boolean autoInstall = ApplicationProperties.getBooleanProperty(AUTO_INSTALL_QUERY, true);
     
+	@Override
     public void afterLoad(Project p) {
-    	if (!p.isMultiUserServer()) {
-    		return;
-    	}
+        if (autoInstall && 
+                p.isMultiUserServer() &&
+                isLuceneTabEnabled(p)) {
+            installQueryFrameStoreOnServer(p);
+        }
+	}
+    
+    private void installQueryFrameStoreOnServer(Project p) {
+        KnowledgeBase kb = p.getKnowledgeBase();
+        new QueryApi(kb).install(LuceneQueryPlugin.getQueryConfiguration(kb, new LuceneConfiguration(kb)));
+    }
+    
+    private boolean isLuceneTabEnabled(Project p) {
         String pluginClassName = LuceneQueryPlugin.class.getCanonicalName();
         WidgetDescriptor desc = p.getTabWidgetDescriptor(pluginClassName);
-        if (desc != null && desc.isVisible()) {
-            KnowledgeBase kb = p.getKnowledgeBase();
-            new QueryApi(kb).install(new QueryConfiguration(kb));
-        }
+        return desc != null && desc.isVisible();
     }
+    
 }
