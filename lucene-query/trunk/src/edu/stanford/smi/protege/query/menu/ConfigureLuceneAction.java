8079424c6a9d72645556e5a2781d56b1a3ae4c7a
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-09-10 21:18:22 +0000

    The new  lucene-query menu together with the lucene query configuration class.

---
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/menu/ConfigureLuceneAction.java
@@ -0,0 +1,33 @@
+package edu.stanford.smi.protege.query.menu;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import edu.stanford.smi.protege.query.LuceneQueryPlugin;
+import edu.stanford.smi.protege.util.ModalDialog;
+import edu.stanford.smi.protegex.owl.ui.icons.OWLIcons;
+
+public class ConfigureLuceneAction extends AbstractAction {
+    private static final long serialVersionUID = -36752085065056828L;
+    
+    private LuceneQueryPlugin plugin;
+    private LuceneConfiguration newConfiguration;
+    
+    public ConfigureLuceneAction(LuceneQueryPlugin plugin) {
+        super("Configure", OWLIcons.getPreferencesIcon());
+        this.plugin = plugin;
+        newConfiguration = new LuceneConfiguration(plugin.getLuceneConfiguration());
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        ConfigureLucenePanel panel = new ConfigureLucenePanel(newConfiguration);
+        int sel = ModalDialog.showDialog(plugin, panel, "Configure Collaborative Protege", ModalDialog.MODE_OK_CANCEL);
+        
+        if (sel == ModalDialog.OPTION_OK) {
+            plugin.setLuceneConfiguration(newConfiguration);
+            newConfiguration.save();
+        }   
+    }
+
+}
