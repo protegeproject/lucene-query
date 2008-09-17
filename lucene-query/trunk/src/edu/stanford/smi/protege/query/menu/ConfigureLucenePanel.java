7c3d5a9044c8992c788b5564b5f723baee3c01c4
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-09-17 22:22:55 +0000

    pass the max matches in from the tabbed configuration panel

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/menu/ConfigureLucenePanel.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/menu/ConfigureLucenePanel.java
@@ -4,8 +4,13 @@ import edu.stanford.smi.protege.util.ValidatableTabComponent;
 
 public class ConfigureLucenePanel  extends ValidatableTabComponent {
     private static final long serialVersionUID = -4965520422316945190L;
+    private ConfigureLuceneTabsPanel tab;
     
     public ConfigureLucenePanel(QueryUIConfiguration configuration) {
-        addTab("Lucene Configuration", new ConfigureLuceneTabsPanel(configuration));       
+        addTab("Lucene Configuration", tab = new ConfigureLuceneTabsPanel(configuration));       
+    }
+    
+    public int getMaxResultsDisplayed() {
+        return tab.getMaxResultsDisplayed();
     }
 }
