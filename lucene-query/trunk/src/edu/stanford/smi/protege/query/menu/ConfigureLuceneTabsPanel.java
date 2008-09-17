c61227c03e9225ffa1bfdb2d811d0d00935cea5f
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2008-09-17 22:18:30 +0000

    refactored and extended

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/menu/ConfigureLuceneTabsPanel.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/menu/ConfigureLuceneTabsPanel.java
@@ -1,23 +1,31 @@
 package edu.stanford.smi.protege.query.menu;
 
-import java.awt.BorderLayout;
-import java.awt.Point;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.text.NumberFormat;
 import java.util.EnumMap;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JCheckBox;
 import javax.swing.JComponent;
-import javax.swing.JScrollPane;
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
+import javax.swing.JTextField;
 
+import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.query.menu.QueryUIConfiguration.BooleanConfigItem;
-import edu.stanford.smi.protege.ui.CheckBoxRenderer;
-import edu.stanford.smi.protege.util.ComponentFactory;
-import edu.stanford.smi.protege.util.ComponentUtilities;
+import edu.stanford.smi.protege.resource.Icons;
+import edu.stanford.smi.protege.ui.DisplayUtilities;
+import edu.stanford.smi.protege.util.AllowableAction;
 import edu.stanford.smi.protege.util.LabeledComponent;
+import edu.stanford.smi.protege.util.Log;
 
 
 
@@ -25,7 +33,7 @@ public class ConfigureLuceneTabsPanel extends JComponent {
     private static final long serialVersionUID = 1835328900385886439L;
     
     private QueryUIConfiguration configuration;
-    private JTable table;
+    private JFormattedTextField maxResultsDisplayedField;
     private EnumMap<BooleanConfigItem, String> titleMap = new EnumMap<BooleanConfigItem, String>(BooleanConfigItem.class);
 
     
@@ -33,19 +41,12 @@ public class ConfigureLuceneTabsPanel extends JComponent {
         this.configuration = configuration;
         initTitles();
         
-        setLayout(new BorderLayout());
-        table = ComponentFactory.createTable(null);
-        table.setModel(createTableModel());
-        table.addMouseListener(new ClickListener());
-        ComponentUtilities.addColumn(table, new DefaultTableCellRenderer());
-        ComponentUtilities.addColumn(table, new CheckBoxRenderer());
-        
-        JScrollPane pane = ComponentFactory.createScrollPane(table);
-        // pane.setColumnHeaderView(table.getTableHeader());
-        // pane.setBackground(table.getBackground());
-        LabeledComponent c = new LabeledComponent("Lucene Configuration", pane);
-        c.add(pane);
-        add(c);
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        addBooleanConfigurationItems();
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        addDefaultSearchSlot();
+        add(Box.createRigidArea(new Dimension(0, 10)));
+        addMaxDisplayed();
     }
     
     private void initTitles() {
@@ -54,41 +55,49 @@ public class ConfigureLuceneTabsPanel extends JComponent {
                      configuration.isOwl() ? "Include Properties in search results" : "Include Slots in search Results");
     }
     
-    private TableModel createTableModel() {
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("Configuration Property");
-        model.addColumn("Value");
-        
-        for (BooleanConfigItem configItem : BooleanConfigItem.values()) {
-            model.addRow(new Object[] {titleMap.get(configItem), configuration.getBooleanConfiguration(configItem) });
-        }    
-        return model;
-    }
+    private void addBooleanConfigurationItems() {
+        for (final BooleanConfigItem configItem : BooleanConfigItem.values()) {
+            boolean enabled = configuration.getBooleanConfiguration(configItem);
+            final JCheckBox box = new JCheckBox(titleMap.get(configItem));
+            box.setSelected(enabled);
+            box.addActionListener(new ActionListener() {
 
-    class ClickListener extends MouseAdapter {
-        public void mousePressed(MouseEvent event) {
-            Point p = event.getPoint();
-            int col = table.columnAtPoint(p);
-            if (col == 1) {
-                int row = table.rowAtPoint(p);                
-                boolean b = getValue(row);
-                setValue(row, !b);                              
-            }
+                public void actionPerformed(ActionEvent e) {
+                    ConfigureLuceneTabsPanel.this.configuration.setBooleanConfiguration(configItem, box.isSelected());
+                }
+                
+            });
+            add(box);
         }
     }
     
-    private boolean getValue(int row) {
-        Boolean b = (Boolean) getTabModel().getValueAt(row, 1);
-        return b.booleanValue();
-    }
+    private void addDefaultSearchSlot() {
+        final JTextField text = new JTextField();
+        text.setText(configuration.getDefaultSlot().getBrowserText());
+        text.enableInputMethods(false);
+        LabeledComponent lc = new LabeledComponent("Default Search Slot", text);
+        lc.addHeaderButton(new AllowableAction("Change Default Slot", Icons.getAddSlotIcon(), null) {
+            private static final long serialVersionUID = 9122206203462592559L;
 
-    private void setValue(int row, boolean value) {
-        getTabModel().setValueAt(Boolean.valueOf(value), row, 1);
-        BooleanConfigItem configItem = BooleanConfigItem.values()[row];
-        configuration.setBooleanConfiguration(configItem, value);
+            public void actionPerformed(ActionEvent e) {
+                Slot newSlot = DisplayUtilities.pickSlot(ConfigureLuceneTabsPanel.this, configuration.getAllSlots());
+                if (newSlot != null) {
+                    configuration.setDefaultSlot(newSlot);
+                    text.setText(newSlot.getBrowserText());
+                }
+            }
+        });
+        add(lc);
     }
     
-    private DefaultTableModel getTabModel() {
-        return (DefaultTableModel) table.getModel();
+    private void addMaxDisplayed() {
+        maxResultsDisplayedField = new JFormattedTextField(NumberFormat.getIntegerInstance());
+        maxResultsDisplayedField.setValue(configuration.getMaxResultsDisplayed());
+        LabeledComponent lc = new LabeledComponent("Max results displayed (-1=all)", maxResultsDisplayedField);
+        add(lc);
+    }
+    
+    public int getMaxResultsDisplayed() {
+        return ((Long) maxResultsDisplayedField.getValue()).intValue();
     }
 }
