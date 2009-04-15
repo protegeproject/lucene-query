c8a226b68c9dcf443d0fea87c65cbc23cf73b15d
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-04-15 22:30:00 +0000

    added visitor methods for the negated and property present queries

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/querytypes/QueryVisitor.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/querytypes/QueryVisitor.java
@@ -3,11 +3,13 @@ package edu.stanford.smi.protege.query.querytypes;
 import edu.stanford.smi.protege.query.querytypes.impl.AndQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.LuceneOwnSlotValueQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.MaxMatchQuery;
+import edu.stanford.smi.protege.query.querytypes.impl.NegatedQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.NestedOwnSlotValueQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.OWLRestrictionQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.OrQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.OwnSlotValueQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.PhoneticQuery;
+import edu.stanford.smi.protege.query.querytypes.impl.PropertyPresentQuery;
 
 public interface QueryVisitor {
     
@@ -23,7 +25,11 @@ public interface QueryVisitor {
     
     void visit(OwnSlotValueQuery q);
     
+    void visit(PropertyPresentQuery q);
+    
     void visit(PhoneticQuery q);
     
     void visit(LuceneOwnSlotValueQuery q);
+    
+    void visit(NegatedQuery q);
 }
