a878fda6eaa9023d1e6b5a3431098363a427f465
tredmond <tredmond@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2009-10-26 21:14:11 +0000

    added owl restriction property present and owl named classes queries.

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/querytypes/QueryVisitor.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/querytypes/QueryVisitor.java
@@ -5,6 +5,8 @@ import edu.stanford.smi.protege.query.querytypes.impl.LuceneOwnSlotValueQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.MaxMatchQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.NegatedQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.NestedOwnSlotValueQuery;
+import edu.stanford.smi.protege.query.querytypes.impl.OWLNamedClassesQuery;
+import edu.stanford.smi.protege.query.querytypes.impl.OWLRestrictionPropertyPresentQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.OWLRestrictionQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.OrQuery;
 import edu.stanford.smi.protege.query.querytypes.impl.OwnSlotValueQuery;
@@ -21,7 +23,11 @@ public interface QueryVisitor {
     
     void visit(NestedOwnSlotValueQuery q);
     
+    void visit(OWLNamedClassesQuery q);
+    
     void visit(OWLRestrictionQuery q);
+    
+    void visit(OWLRestrictionPropertyPresentQuery q);
     
     void visit(OwnSlotValueQuery q);
     
