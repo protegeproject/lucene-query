f2731e0fbf73e35aa55a8a0f53c706ffd2f90872
Tania Tudorache <tudorache@users.noreply.github.com>
2010-09-22 18:21:35 +0000

    Tim made the indexable frames configurable. We'll need to improve this later.

--- src/edu/stanford/smi/protege/query/indexer/AbstractIndexer.java
+++ src/edu/stanford/smi/protege/query/indexer/AbstractIndexer.java
@@ -37,92 +37,92 @@ import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
 import edu.stanford.smi.protege.query.util.FutureTask;
 import edu.stanford.smi.protege.query.util.IndexTaskRunner;
 import edu.stanford.smi.protege.util.Log;
+import edu.stanford.smi.protegex.owl.model.OWLAnonymousClass;
 import edu.stanford.smi.protegex.owl.model.RDFResource;
 
 public abstract class AbstractIndexer implements Indexer {
     private static final long serialVersionUID = 1964188756235468692L;
 
 private transient static final Logger log = Log.getLogger(AbstractIndexer.class);
-  
+
   private enum Status {
     INDEXING, READY, DOWN
   };
-  
+
   private Object kbLock;
-  
+
   private String baseIndexPath;
-  
+
   private Analyzer analyzer;
   private NarrowFrameStore delegate;
   private Status status = Status.INDEXING;
-  
+
   private Set<Slot> searchableSlots;
-  
+
   boolean owlMode = false;
-  
+
   private Slot nameSlot;
-  
+
   public static final String FRAME_NAME                = "frameName";
   public static final String SLOT_NAME                 = "slotName";
   public static final String BROWSER_TEXT              = "browserText";
   public static final String CONTENTS_FIELD            = "contents";
   public static final String LITERAL_CONTENTS          = "literalContents";
-  
+
   private transient IndexTaskRunner indexRunner;
 
-  
+
   public AbstractIndexer() {
       analyzer = createAnalyzer();
       indexRunner = new IndexTaskRunner();
       indexRunner.startBackgroundThread();
   }
-  
+
   public void dispose() {
       indexRunner.dispose();
   }
-  
+
   public void setSearchableSlots(Set<Slot> searchableSlots) {
       this.searchableSlots = searchableSlots;
   }
-  
+
   public void setBaseIndexPath(String baseIndexPath) {
       this.baseIndexPath = baseIndexPath;
   }
-  
+
   private String getFullIndexPath() {
       return baseIndexPath + File.separator + relativeIndexLocation();
     }
-  
+
   public void setOWLMode(boolean owlMode) {
       this.owlMode = owlMode;
   }
-  
+
   public void setNarrowFrameStoreDelegate(NarrowFrameStore delegate) {
       this.delegate = delegate;
       nameSlot = (Slot) delegate.getFrame(Model.SlotID.NAME);
   }
-  
+
   public void setKnowledgeBase(KnowledgeBase kb) {
       kbLock = kb;
   }
-  
+
   protected Analyzer getAnalyzer() {
       return analyzer;
   }
-  
+
   protected IndexTaskRunner getIndexRunner() {
       return indexRunner;
   }
-  
+
   protected abstract Analyzer createAnalyzer();
 
   protected IndexWriter openWriter(boolean create) throws IOException {
     return new IndexWriter(getFullIndexPath(), analyzer, create);
   }
-  
 
-  
-  @SuppressWarnings("unchecked")
+
+
   public void indexOntologies() throws ProtegeException {
       FutureTask indexTask = new FutureTask() {
           public void run() {
@@ -137,11 +137,13 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
                       frames = delegate.getFrames();
                   }
                   for (Frame frame : frames) {
-                    errorsFound = errorsFound || !addFrame(myWriter, frame);
+                      if (indexable(frame)) {
+                          errorsFound = errorsFound || !addFrame(myWriter, frame);
+                      }
                   }
                   myWriter.optimize();
                   status = Status.READY;
-                  Log.getLogger().info("Finished indexing ontology (" 
+                  Log.getLogger().info("Finished indexing ontology ("
                                        + ((System.currentTimeMillis() - start)/1000) + " seconds)");
               } catch (IOException ioe) {
                   died(ioe);
@@ -163,12 +165,16 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
           throw new RuntimeException(interrupt);
       }
   }
-  
+
+  public boolean indexable(Frame frame) {
+      return !frame.isSystem() &&  !(frame instanceof OWLAnonymousClass);
+  }
+
   protected boolean addFrameBrowserText(IndexWriter writer, Frame frame) {
       boolean errorsFound = false;
       try {
           Document doc = new Document();
-          doc.add(new Field(FRAME_NAME, getFrameName(frame), 
+          doc.add(new Field(FRAME_NAME, getFrameName(frame),
                             Field.Store.YES, Field.Index.UN_TOKENIZED));
           doc.add(new Field(BROWSER_TEXT, frame.getBrowserText(), Field.Store.YES, Field.Index.TOKENIZED));
           writer.addDocument(doc);
@@ -179,7 +185,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       }
       return errorsFound;
   }
-  
+
   @SuppressWarnings("unchecked")
   private boolean addFrame(IndexWriter writer, Frame frame) {
       boolean errorsFound = addFrameBrowserText(writer, frame);
@@ -212,7 +218,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       }
       return !errorsFound;
   }
-  
+
   protected void addUpdate(IndexWriter writer, Frame frame, Slot slot, String value) throws IOException {
     if (owlMode && value.startsWith("~#")) {
       value = value.substring(5);
@@ -221,7 +227,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       return;
     }
     Document doc = new Document();
-    doc.add(new Field(FRAME_NAME, getFrameName(frame), 
+    doc.add(new Field(FRAME_NAME, getFrameName(frame),
                       Field.Store.YES, Field.Index.UN_TOKENIZED));
     if (slot != null) {
         doc.add(new Field(SLOT_NAME, getFrameName(slot),
@@ -231,11 +237,11 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
     doc.add(new Field(LITERAL_CONTENTS, value, Field.Store.YES, Field.Index.UN_TOKENIZED));
     writer.addDocument(doc);
   }
-  
+
   public Collection<Frame> executeQuery(Collection<Slot> slots, String expr) throws IOException {
       return executeQuery(generateLuceneQuery(slots, expr));
   }
-  
+
   public Collection<Frame> executeQuery(final Query luceneQuery) throws IOException {
       FutureTask queryTask = new FutureTask() {
           public void run() {
@@ -280,10 +286,10 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
   }
 
 
-  
+
   /*
    * This is one of the queries that is actually used for searching the lucene index
-   * rather than just for maintaining it.  It is a slight variation of the query that 
+   * rather than just for maintaining it.  It is a slight variation of the query that
    * just searches for a single slot.
    */
   protected Query generateLuceneQuery(Collection<Slot> slots, String expr) throws IOException {
@@ -315,17 +321,17 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
     }
     return query;
   }
- 
-  
+
+
   /*
    * This query already contains the frame name to find.  It is used to find a document and thence delete it.
    */
   protected Query generateLuceneQuery(Frame frame, Slot slot, String literalValue) throws IOException {
       BooleanQuery query = new  BooleanQuery();
-      
+
       Term term = new Term(LITERAL_CONTENTS, literalValue);
       query.add(new TermQuery(term), BooleanClause.Occur.MUST);
-      
+
       term = new Term(FRAME_NAME, getFrameName(frame));
       query.add(new TermQuery(term), BooleanClause.Occur.MUST);
 
@@ -333,28 +339,28 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       query.add(new TermQuery(term), BooleanClause.Occur.MUST);
       return query;
     }
-  
+
   /*
    * This query already contains the name of the document that it is looking for.
    * It is used to find a document so that it can be deleted.
    */
   protected Query generateLuceneQuery(String fname) throws IOException {
     BooleanQuery query  = new  BooleanQuery();
-    
+
     Term term;
     term = new Term(FRAME_NAME, fname);
     query.add(new TermQuery(term), BooleanClause.Occur.MUST);
-    
+
     return query;
   }
-  
+
   /*
    * This query already contains the frame name of the document it is looking for.
    * It is used to find a document so that it can be deleted.
    */
   protected Query generateLuceneQuery(Frame frame, Slot slot) throws IOException {
     BooleanQuery query  = new  BooleanQuery();
-    
+
     Term term;
     term = new Term(FRAME_NAME, getFrameName(frame));
     query.add(new TermQuery(term), BooleanClause.Occur.MUST);
@@ -362,14 +368,14 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
     query.add(new TermQuery(term), BooleanClause.Occur.MUST);
     return query;
   }
-  
+
   protected void died(IOException ioe) {
     Log.getLogger().warning("Search index update failed " + ioe);
     Log.getLogger().warning("This exception will not interfere with normal (non-query) operations");
     Log.getLogger().warning("suggest reindexing to get the lucene indicies back");
     status = Status.DOWN;
   }
-  
+
   private void forceClose(Searcher searcher) {
     try {
       if (searcher != null) {
@@ -379,7 +385,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       Log.getLogger().log(Level.WARNING, "Exception caught closing files involved during search", ioe);
     }
   }
-  
+
   private void forceClose(IndexReader reader) {
       try {
           if (reader != null) {
@@ -389,7 +395,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
           Log.getLogger().log(Level.WARNING, "Exception caught reading/deleting documents from index", ioe);
       }
   }
-  
+
   protected void forceClose(IndexWriter writer) {
     try {
       if (writer != null) {
@@ -399,11 +405,11 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       Log.getLogger().log(Level.WARNING, "Exception caught closing files involved in lucene indicies", ioe);
     }
   }
-  
+
   private String getFrameName(Frame frame) {
 	  return frame.getFrameID().getName();
   }
-  
+
   private Frame getFrameByName(String name) {
     Set<Frame> frames;
     synchronized (kbLock) {
@@ -411,19 +417,19 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
     }
     return frames.iterator().next();
   }
-  
+
   private boolean isAnonymous(Frame frame) {
     if (!owlMode) {
       return false;
     }
-    
+
     String name = getFrameName(frame);
     if (name == null) {
       return true;
     }
     return name.startsWith("@");
   }
-  
+
   private void installOptimizeTask() {
       indexRunner.setCleanUpTask(new Runnable() {
           public void run() {
@@ -439,7 +445,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
           }
       });
   }
-  
+
   protected void deleteDocuments(Query q) throws IOException {
       List<Integer> deletions = new ArrayList<Integer>();
       IndexSearcher searcher = null;
@@ -466,17 +472,17 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
           forceClose(reader);
       }
       if (log.isLoggable(Level.FINE)) {
-          log.fine("Delete lucene document operation for query " + q  + " took " + 
+          log.fine("Delete lucene document operation for query " + q  + " took " +
                    (System.currentTimeMillis() - start) + "ms");
       }
   }
-  
-  
+
+
   /* --------------------------------------------------------------------------
    * Update Utilities for the Narrow Frame Store
    */
-  
-  public void addValues(final Frame frame, final Slot slot, final Collection values) { 
+
+  public void addValues(final Frame frame, final Slot slot, final Collection values) {
       if (status == Status.DOWN || !searchableSlots.contains(slot) || isAnonymous(frame)) {
           return;
       }
@@ -515,7 +521,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       });
       installOptimizeTask();
   }
-  
+
   public void removeValue(final Frame frame, final Slot slot, final Object value) {
       if (status == Status.DOWN || !searchableSlots.contains(slot) || !(value instanceof String)) {
           return;
@@ -539,7 +545,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       });
       installOptimizeTask();
   }
-  
+
   public void removeValues(final Frame frame, final Slot slot) {
       if (status == Status.DOWN || !searchableSlots.contains(slot)) {
           return;
@@ -566,12 +572,12 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
                   died(ioe);
               } catch (Throwable t) {
                   Log.getLogger().warning("Exception caught during indexing " + t);
-              } 
+              }
           }
       });
       installOptimizeTask();
   }
-  
+
   public void removeValues(final String fname) {
       if (status == Status.DOWN) {
           return;
@@ -595,7 +601,7 @@ private transient static final Logger log = Log.getLogger(AbstractIndexer.class)
       });
       installOptimizeTask();
   }
-  
+
   public void localize(KnowledgeBase kb) {
       kbLock = kb;
       analyzer = createAnalyzer();
