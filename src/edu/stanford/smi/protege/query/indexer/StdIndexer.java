c7c8ee9c14b4f7c4154b32ef0a7e25ab20f05a76
Timothy Redmond <1761044+stdotjohn@users.noreply.github.com>
2010-11-09 19:12:38 +0000

    Removed the funky future task and index task runner.  In its place I am now using the Java 5 Executors.  It seems quite possible that the old IndexTaskRunner contributed to the lucene index corruption.  It was quite complicated.  I have not yet seen another cause for this problem.

--- src/edu/stanford/smi/protege/query/indexer/StdIndexer.java
+++ src/edu/stanford/smi/protege/query/indexer/StdIndexer.java
@@ -2,8 +2,10 @@ package edu.stanford.smi.protege.query.indexer;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.lucene.analysis.Analyzer;
@@ -24,7 +26,6 @@ import org.apache.lucene.search.TermQuery;
 import edu.stanford.smi.protege.model.Frame;
 import edu.stanford.smi.protege.query.querytypes.impl.LuceneBrowserTextSearch;
 import edu.stanford.smi.protege.query.querytypes.impl.LuceneOwnSlotValueQuery;
-import edu.stanford.smi.protege.query.util.FutureTask;
 import edu.stanford.smi.protege.util.Log;
 
 
@@ -58,80 +59,55 @@ public class StdIndexer extends AbstractIndexer {
   }
   
   public void browserTextChanged(final Frame frame){
-      getIndexRunner().addTask(new FutureTask() {
-          public void run() {
-              BooleanQuery query  = new  BooleanQuery();
+      getIndexRunner().submit(new BrowserTextChangedRunnable(frame) , true);
 
-              Term term;
-              term = new Term(FRAME_NAME, frame.getName());
-              query.add(new TermQuery(term), BooleanClause.Occur.MUST);
-              query.add(new TermQuery(new Term(BROWSER_TEXT_PRESENT, Boolean.TRUE.toString())), BooleanClause.Occur.MUST);
-              try {
-                  deleteDocuments(query);
-              }
-              catch (IOException ioe) {
-                  died(ioe);
-                  return;
-              }
-              IndexWriter writer = null;
-              try {
-                  writer = openWriter(false);
-                  addFrameBrowserText(writer, frame);
-              } catch (IOException ioe) {
-                  died(ioe);
-              } catch (Throwable t) {
-                  Log.getLogger().warning("Error during indexing" + t);
-              }
-              finally {
-                  forceClose(writer);
-              }
+  }
+
+  private class BrowserTextChangedRunnable implements Runnable {
+      private Frame frame;
+
+      public BrowserTextChangedRunnable(Frame frame) {
+          this.frame = frame;
+      }
+      public void run() {
+          BooleanQuery query  = new  BooleanQuery();
+
+          Term term;
+          term = new Term(FRAME_NAME, frame.getName());
+          query.add(new TermQuery(term), BooleanClause.Occur.MUST);
+          query.add(new TermQuery(new Term(BROWSER_TEXT_PRESENT, Boolean.TRUE.toString())), BooleanClause.Occur.MUST);
+          try {
+              deleteDocuments(query);
           }
-      });
-
+          catch (IOException ioe) {
+              died(ioe);
+              return;
+          }
+          IndexWriter writer = null;
+          try {
+              writer = openWriter(false);
+              addFrameBrowserText(writer, frame);
+          } catch (IOException ioe) {
+              died(ioe);
+          } catch (Throwable t) {
+              Log.getLogger().warning("Error during indexing" + t);
+          }
+          finally {
+              forceClose(writer);
+          }
+      }
   }
   
   public Map<String, String> queryBrowserText(String luceneQuery) throws IOException {
       QueryParser parser = new QueryParser(BROWSER_TEXT, getAnalyzer());
       parser.setAllowLeadingWildcard(true);
-      FutureTask queryTask;
       try {
           final Query query = parser.parse(luceneQuery);
-          queryTask = new FutureTask() {
-              public void run() {
-                  if (getStatus() == Status.DOWN) {
-                      set(null);
-                  }
-                  Searcher searcher = null;
-                  Map<String, String> results = new TreeMap<String, String>();
-                  try {
-                      searcher = new IndexSearcher(getFullIndexPath());
-                      Hits hits = searcher.search(query);
-                      for (int i = 0; i < hits.length(); i++) {
-                          Document doc = hits.doc(i);
-                          String frameName = doc.get(FRAME_NAME);
-                          String browserText = doc.get(BROWSER_TEXT);
-                          synchronized (kbLock) {
-                              results.put(browserText, frameName);
-                          }
-                      }
-                  } catch (IOException ioe) {
-                      setException(ioe);
-                  } finally {
-                      forceClose(searcher);
-                  }
-                  set(results);
-              }
-          };
-
+          return getIndexRunner().submit(new QueryBrowserTextCallable(query)).get();
       } catch (ParseException e) {
           IOException ioe = new IOException(e.getMessage());
           ioe.initCause(e);
           throw ioe;
-      }
-
-      try {
-          getIndexRunner().addTask(queryTask);
-          return (Map<String, String>) queryTask.get();
       } catch (InterruptedException e) {
           throw new RuntimeException(e);
       } catch (ExecutionException e) {
@@ -144,7 +120,38 @@ public class StdIndexer extends AbstractIndexer {
           }
       }
   }
-  
+
+  private class QueryBrowserTextCallable implements Callable<Map<String, String>> {
+      private Query query;
+
+      public QueryBrowserTextCallable(Query query) {
+          this.query = query;
+      }
+
+      public Map<String, String> call() throws IOException {
+          if (getStatus() == Status.DOWN) {
+              return new HashMap<String, String>();
+          }
+          Searcher searcher = null;
+          Map<String, String> results = new TreeMap<String, String>();
+          try {
+              searcher = new IndexSearcher(getFullIndexPath());
+              Hits hits = searcher.search(query);
+              for (int i = 0; i < hits.length(); i++) {
+                  Document doc = hits.doc(i);
+                  String frameName = doc.get(FRAME_NAME);
+                  String browserText = doc.get(BROWSER_TEXT);
+                  synchronized (kbLock) {
+                      results.put(browserText, frameName);
+                  }
+              }
+          } finally {
+              forceClose(searcher);
+          }
+          return results;
+      }
+  }
+
   public String relativeIndexLocation() {
       return "Standard";
   }
