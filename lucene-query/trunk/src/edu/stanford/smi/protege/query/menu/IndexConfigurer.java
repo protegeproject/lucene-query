e055c4c8df62cd69b0ae15391cd0202ba516f260
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-12-23 16:14:36 +0000

    added to the configuration the indexed frame types

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/menu/IndexConfigurer.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/menu/IndexConfigurer.java
@@ -13,12 +13,14 @@ import java.util.Set;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 import edu.stanford.smi.protege.model.KnowledgeBase;
 import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.query.api.QueryConfiguration;
+import edu.stanford.smi.protege.query.indexer.AbstractIndexer;
 import edu.stanford.smi.protege.query.indexer.IndexMechanism;
 import edu.stanford.smi.protege.resource.Icons;
 import edu.stanford.smi.protege.ui.DisplayUtilities;
@@ -32,25 +34,36 @@ import edu.stanford.smi.protege.util.SelectableList;
 
 public class IndexConfigurer extends JPanel {
     private static final long serialVersionUID = -4406193294688623588L;
-    
+
+    /*
+     * Actions in this dialog are written right away to the QueryConfiguration object
+     * and not only when the user clicks on OK. Behavior is not ideal.
+     */
+
     private QueryConfiguration configuration;
     private List<Slot> allSlots;
-    
+
+
     public IndexConfigurer(KnowledgeBase kb) {
         configuration = new QueryConfiguration(kb);
         allSlots = new ArrayList<Slot>(configuration.getSearchableSlots());
         Collections.sort(allSlots);
         allSlots = Collections.unmodifiableList(allSlots);
+
         createGui();
     }
-    
+
     private void createGui() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
         add(getSlotsListComponent());
+
         add(Box.createRigidArea(new Dimension(0, 10)));
         for (final IndexMechanism mechanism : IndexMechanism.values()) {
+
             boolean defaultValue = mechanism.isEnabledByDefault();
             mechanism.setEnabled(configuration, defaultValue);
+
             final JCheckBox box = new JCheckBox(mechanism.getDescription());
             box.setSelected(defaultValue);
             box.setAlignmentX(Component.LEFT_ALIGNMENT);
@@ -59,11 +72,15 @@ public class IndexConfigurer extends JPanel {
                 public void actionPerformed(ActionEvent e) {
                     mechanism.setEnabled(configuration, box.isSelected());
                 }
-                
+
             });
             add(box);
         }
+
+        add(Box.createVerticalStrut(20));
+        add(getIndexedFrameTypesComponent());
     }
+
 
     private LabeledComponent getSlotsListComponent() {
         final SelectableList slotsList = ComponentFactory.createSelectableList(null);
@@ -73,7 +90,7 @@ public class IndexConfigurer extends JPanel {
         Collections.sort(searchableSlots);
         ComponentUtilities.addListValues(slotsList, searchableSlots);
 
-        final LabeledComponent labeledComp = new LabeledComponent("Slots to export", new JScrollPane(slotsList), true );
+        final LabeledComponent labeledComp = new LabeledComponent("Slots/Properties to index:", new JScrollPane(slotsList), true );
 
         labeledComp.addHeaderButton(new AllowableAction("Add slots", Icons.getAddSlotIcon(), null) {
 
@@ -85,9 +102,9 @@ public class IndexConfigurer extends JPanel {
                     return;
                 }
                 // Show util window for multiple slot selection
-                Collection<Slot> newSlots = DisplayUtilities.pickSlots(IndexConfigurer.this, 
-                                                                       removeableSlots, 
-                                                                       "Select slots to export (multiple selection)");
+                Collection<Slot> newSlots = DisplayUtilities.pickSlots(IndexConfigurer.this,
+                                                                       removeableSlots,
+                                                                       "Select slots to index (multiple selection)");
                 Set<Slot> searchableSlots = configuration.getSearchableSlots();
                 searchableSlots.addAll(newSlots);
                 configuration.setSearchableSlots(searchableSlots);
@@ -113,13 +130,62 @@ public class IndexConfigurer extends JPanel {
             }
 
         });
-        
+
         labeledComp.setAlignmentX(Component.LEFT_ALIGNMENT);
         return labeledComp;
     }
-    
+
+    private Component getIndexedFrameTypesComponent() {
+        Box box = new Box(BoxLayout.Y_AXIS);
+        box.add(new JLabel("<html><body><b>Index following frame types: </b></body></html>"));
+
+        final JCheckBox classesCheckBox = new JCheckBox("Classes");
+        classesCheckBox.setSelected(configuration.isIndexedClasses());
+        classesCheckBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        classesCheckBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (classesCheckBox.isSelected()) {
+                    configuration.getIndexedFrameTypes().add(AbstractIndexer.FRAME_TYPE_CLS);
+                } else {
+                    configuration.getIndexedFrameTypes().remove(AbstractIndexer.FRAME_TYPE_CLS);
+                }
+            }
+        });
+        box.add(classesCheckBox);
+
+        final JCheckBox slotsCheckBox = new JCheckBox("Slots/Properties");
+        slotsCheckBox.setSelected(configuration.isIndexedSlots());
+        slotsCheckBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        slotsCheckBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (slotsCheckBox.isSelected()) {
+                    configuration.getIndexedFrameTypes().add(AbstractIndexer.FRAME_TYPE_SLOT);
+                } else {
+                    configuration.getIndexedFrameTypes().remove(AbstractIndexer.FRAME_TYPE_SLOT);
+                }
+            }
+        });
+        box.add(slotsCheckBox);
+
+        final JCheckBox instancesCheckBox = new JCheckBox("Instances/Individuals");
+        instancesCheckBox.setSelected(configuration.isIndexedInstances());
+        instancesCheckBox.setAlignmentX(Component.LEFT_ALIGNMENT);
+        instancesCheckBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (instancesCheckBox.isSelected()) {
+                    configuration.getIndexedFrameTypes().add(AbstractIndexer.FRAME_TYPE_INSTANCE);
+                } else {
+                    configuration.getIndexedFrameTypes().remove(AbstractIndexer.FRAME_TYPE_INSTANCE);
+                }
+            }
+        });
+        box.add(instancesCheckBox);
+
+        return box;
+    }
+
     public QueryConfiguration getConfiguration() {
         return configuration;
     }
-    
+
 }
