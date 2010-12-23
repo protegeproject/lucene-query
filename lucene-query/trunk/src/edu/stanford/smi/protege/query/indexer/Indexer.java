c3fc074ce19dd9439e56af0d50fa6ab41a291adc
tudorache <tudorache@30a32298-b1ee-0310-87e3-a7336bc3aa68>
2010-12-23 16:11:37 +0000

    added interface setIndexedFrameTypes

--- lucene-query/trunk/src/edu/stanford/smi/protege/query/indexer/Indexer.java
+++ lucene-query/trunk/src/edu/stanford/smi/protege/query/indexer/Indexer.java
@@ -10,31 +10,33 @@ import edu.stanford.smi.protege.model.Slot;
 import edu.stanford.smi.protege.model.framestore.NarrowFrameStore;
 
 public interface Indexer {
-    
+
     void indexOntologies() throws ProtegeException;
-    
-    
+
+
     void setSearchableSlots(Set<Slot> slots);
-    
+
+    void setIndexedFrameTypes(Set<String> types);
+
     void setNarrowFrameStoreDelegate(NarrowFrameStore nfs);
-    
+
     void setKnowledgeBase(KnowledgeBase kb);
-    
+
     void setOWLMode(boolean owlMode);
-    
+
     void setBaseIndexPath(String path);
-    
-    
-    
+
+
+
     void addValues(final Frame frame, final Slot slot, final Collection values);
-    
+
     void removeValue(final Frame frame, final Slot slot, final Object value);
-    
+
     void removeValues(final Frame frame, final Slot slot);
-    
+
     void removeValues(final String fname);
-    
-    
+
+
     String relativeIndexLocation();
 
     void dispose();
